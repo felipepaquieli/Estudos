3885d3c70171c6433e5507a3a12ded84e9ed44d2
Felipe <felipepaquieli@gmail.com>
2020-09-01 08:15:49 -0300

    teste

---
+++ GameTOP/JogoFODA.cs
@@ -0,0 +1,29 @@
+using System;
+using GameTOP.Interface;
+
+namespace GameTOP
+{
+    public class JogoFODA
+    {
+        private readonly iJogador _jogadorA;
+        private readonly iJogador _jogadorB;
+
+        public JogoFODA(iJogador jogadorA, iJogador jogadorB)
+        {
+            _jogadorA = jogadorA;
+            _jogadorB = jogadorB;
+        }
+        public void IniciarJogo()
+        {
+            Console.Write(_jogadorA.Corre());
+            Console.Write(_jogadorA.Chuta());
+            Console.Write(_jogadorA.Passe());
+
+            Console.Write("Proximo Jogador \n");
+
+            Console.Write(_jogadorB.Corre());
+            Console.Write(_jogadorB.Chuta());
+            Console.Write(_jogadorB.Passe());
+        }
+    }
+}
